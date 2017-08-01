d64a10c8c3b917590c338d066b63c4d9c1cdda51
vargo <jvargo@gmail.com>
2017-08-01 10:15:07 -0500

    added files

---
+++ linkOLNwMode.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+
+file <- "~/Documents/GitHub/ACS_ITHIMr/data/NHTS_2009_transfer_US.txt"
+
+OLN <- read.table(file, sep="\t", as.is = T, skip=319, stringsAsFactors=FALSE)
+OLNheader <- read.table(file, sep="\t", as.is = T, nrows = 1)
+colnames(OLN) <- unlist(OLNheader)
+
+modeFile <- "~/Documents/GitHub/ACS_ITHIMr/data/NHTS_clusterByMode.csv"
+
+NHTSmode <- read.csv(modeFile, header=T) %>% spread()
+
+
