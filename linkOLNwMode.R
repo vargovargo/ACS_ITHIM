ee2d76a0e9e106ddebe081ec2f6f5036b408abac
vargo <jvargo@gmail.com>
2017-08-01 11:16:26 -0500

    fix path, bring in acsapi

--- linkOLNwMode.R
+++ linkOLNwMode.R
@@ -1,12 +1,12 @@
 library(tidyverse)
 
-file <- "~/Documents/GitHub/ACS_ITHIMr/data/NHTS_2009_transfer_US.txt"
+file <- "~/Documents/GitHub/ACS_ITHIM/data/NHTS_2009_transfer_US.txt"
 
 OLN <- read.table(file, sep="\t", as.is = T, skip=319, stringsAsFactors=FALSE)
 OLNheader <- read.table(file, sep="\t", as.is = T, nrows = 1)
 colnames(OLN) <- unlist(OLNheader)
 
-modeFile <- "~/Documents/GitHub/ACS_ITHIMr/data/NHTS_clusterByMode.csv"
+modeFile <- "~/Documents/GitHub/ACS_ITHIM/data/NHTS_clusterByMode.csv"
 
 NHTSmode <- read.csv(modeFile, header=T) %>% spread()
 
