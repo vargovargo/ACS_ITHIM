9cf6b59894c1bb1c4f5077818de92d56944c0cba
vargo <jvargo@gmail.com>
2017-08-18 09:12:48 -0500

    fix lookup

--- archive/countyMSAlookup.R
+++ archive/countyMSAlookup.R
@@ -2,7 +2,7 @@ rm(list=ls())
 library(jsonlite)
 library(tidyverse)
 
-grabFIPS <- function() {
+lookupCounty <- function(word) {
 
   countyList <-  as.data.frame(fromJSON("https://api.census.gov/data/2015/acs5?get=NAME&for=county:*&in=state:*&key=f78d6b6c18608edc379b5a06c55407ceb45e7038")) %>% 
     .[-1,] %>% 
@@ -11,16 +11,18 @@ grabFIPS <- function() {
     select(county, state, V1, V2, V3)
   
   names(countyList) <- c("county","state","countyState","countyFIPS","stateFIPS")
-    
+ 
+  return(countyList[grep(word, countyList$countyState),])
+  
+}
+
+lookupMSA <- function(word) {
+  
   msaList <- as.data.frame(fromJSON("http://api.census.gov/data/2015/acs5?get=NAME&for=metropolitan+statistical+area/micropolitan+statistical+area:*&key=f78d6b6c18608edc379b5a06c55407ceb45e7038")) %>%
     .[-1,]
   
   names(msaList) <- c("msa","msaFIPS")
   
-  return(as.list(countyList, msaList))
+  return(msaList[grep(word, msaList$msa),])
   
 }
-
-# Cnty <- getTractAgeSex(state = 55,county = 025) 
-
-FIPS <- grabFIPS()
